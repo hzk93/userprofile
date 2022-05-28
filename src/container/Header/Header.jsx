8888a2ec9c2a859a1bf3a34bd52ab819685c942a
hzk93 <hassanzk93@gmail.com>
2022-05-28 16:05:59 +0500

    updated profile information

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -34,7 +34,8 @@ const Header = () => {
            </div>
           <div className='tag-cmp app__flex'>
           <p className='p-text'>Web Developer</p>
-          <p className='p-text'>Freelancer</p>
+          <p className='p-text'>MERN Stack Developer</p>
+          <p className='p-text'>React/React Native Developer</p>
           </div>
          </div> 
         </motion.div>
