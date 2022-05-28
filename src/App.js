b735de230b9c8ccd86031eadeeed1a47027974ad
hzk93 <hassanzk93@gmail.com>
2022-05-28 10:32:19 +0500

    removed testimonial section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
  import React from 'react';
 
- import {About, Footer, Header, Skills, Testimonial, Work} from './container';
+ import {About, Footer, Header, Skills, Work} from './container';  //Exluded Testimonial Section//
  import { Navbar } from './components';
  import './App.scss';
  const App = () => {
      return(
         <div className='app'>
-            <h1>Testing Auto Deployment</h1>
             <Navbar />
             <Header /> 
             <About />
             <Work />
             <Skills />
-            <Testimonial />
+            {/* <Testimonial /> */}
             <Footer />
 
         </div>
